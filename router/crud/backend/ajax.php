d3b96c66917cf4190da1b077a1d81d4f51e47f0b
Rost <rostslav21@gmail.com>
2023-08-22 22:30:42 +0300

    Done router

---
+++ router/crud/backend/ajax.php
@@ -0,0 +1,88 @@
+<?php
+$file = "file.json";
+
+if($_REQUEST['type'] == 'get'){
+
+    //Данные из файла
+    $json = file_get_contents($file);
+
+    //Данные которые пришли
+    $postdata = json_decode(file_get_contents("php://input"), true);
+    if(isset($postdata['id'])){
+        $arRes = array();
+
+        $arCurItems = json_decode($json);
+
+        $found_key = array_search($postdata['id'], array_column($arCurItems, 'id'));
+        $arRes = $arCurItems[$found_key];
+
+        $json = json_encode($arRes);
+    }
+
+    echo $json;
+}
+
+
+if($_REQUEST['type'] == 'set'){
+
+    //Данные которые пришли
+    $postdata = json_decode(file_get_contents("php://input"), true);
+
+    //Фомируем массив для записи
+    $newItem = array(
+        'id' => uniqid(),
+        'content' => $postdata['text']
+    );
+
+    //Получаем текущие данные
+    $arCurItems = json_decode(file_get_contents($file), true);
+
+    $arCurItems[] = $newItem;
+    
+    file_put_contents($file, json_encode($arCurItems));
+
+    $json = file_get_contents($file);
+
+    echo $json;
+}
+
+if($_REQUEST['type'] == 'del'){
+
+    //Данные которые пришли
+    $postdata = json_decode(file_get_contents("php://input"), true);
+
+    //Получаем текущие данные
+    $arCurItems = json_decode(file_get_contents($file), true);
+
+    $found_key = array_search($postdata['id'], array_column($arCurItems, 'id'));
+    unset($arCurItems[$found_key]);
+
+    $arCurItemsNew = array_values($arCurItems);
+    
+    file_put_contents($file, json_encode($arCurItemsNew));
+
+    $json = file_get_contents($file);
+
+    echo $json;
+}
+
+if($_REQUEST['type'] == 'upd'){
+
+    //Данные которые пришли
+    $postdata = json_decode(file_get_contents("php://input"), true);
+
+    //Получаем текущие данные
+    $arCurItems = json_decode(file_get_contents($file), true);
+
+    $found_key = array_search($postdata['id'], array_column($arCurItems, 'id'));
+    $arCurItems[$found_key]['content'] = $postdata['text'];
+
+    $arCurItemsNew = array_values($arCurItems);
+    
+    file_put_contents($file, json_encode($arCurItemsNew));
+
+    $json = file_get_contents($file);
+
+    echo $json;
+}
+?>
